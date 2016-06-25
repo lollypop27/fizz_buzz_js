65a3265a4c3fd3e5de66e96270d55fe43f3bd088
lollypop27 <lara@adafctory.co.za>
2016-06-25 15:16:49 +0200

    added same number if rquirement not filled

--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -4,4 +4,8 @@ describe("FizzBuzz", function() {
   beforeEach(function() {
     fizz_buzz = new FizzBuzz();
   });
+
+  it("returns 1 if number = 1" function() {
+    expect(fizz_buzz(1)).toEqual('1');
+  });
 });
