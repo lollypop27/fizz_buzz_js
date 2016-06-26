8e6574f3bd5ea7d21ba5973d659d9eaf13eff29a
lollypop27 <lara@adafctory.co.za>
2016-06-26 09:24:32 +0200

    adding fizzbuzz % 15 equation

---
+++ assets/js/fizzbuzz.js
@@ -0,0 +1,10 @@
+function fizz_buzz(number) {
+
+    if (nummber % 15 == 0) {
+        obj.Message = "fizzbuzz" }
+    else if (nummber % 3 == 0) {
+        obj.Message = "fizz" }
+    else if (number % 5 == 0) {
+        obj,Message = "buzz" }
+    }
+};
