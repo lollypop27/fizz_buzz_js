1447b6a4002090f6cdd7bafbd90f572d2e0778b3
lollypop27 <lara@adafctory.co.za>
2016-06-26 10:38:56 +0200

    updating code for equation output

--- assets/js/fizzbuzz.js
+++ assets/js/fizzbuzz.js
@@ -1,13 +1,24 @@
-function fizz_buzz(number) {
-  if (number <= 0) {
-    return new Error("Try again your number must be greater than 0");
-  else if (nummber % 15 == 0) {
-      obj.Message = "fizzbuzz"; }
-  else if (nummber % 3 == 0) {
-      obj.Message = "fizz"; }
-  else if (number % 5 == 0) {
-      obj,Message = "buzz"; }
-  else {
-    obj.Message = number;
-  }
-};
+for (var i=1; i <= 100; i++)
+{
+    if (i % 15 == 0)
+        console.log("fizzbuzz");
+    else if (i % 3 == 0)
+        console.log("fizz");
+    else if (i % 5 == 0)
+        console.log("buzz");
+    else
+        console.log(i);
+}
+// function fizz_buzz(number) {
+//   if (number <= 0) {
+//     return new Error("Try again your number must be greater than 0");
+//   else if (nummber % 3 == 0) {
+//       obj.Message = "fizz"; }
+//   else if (number % 5 == 0) {
+//       obj,Message = "buzz"; }
+//   else if (nummber % 15 == 0) {
+//       obj.Message = "fizzbuzz"; }
+//   else {
+//     return = number;
+//   }
+// };
--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -1,23 +1,27 @@
 describe("fizz_buzz", function() {
-    var fizz_buzz;
+  var game;
 
-    it("returns number if no conditions are met", function() {
-      expect(fizz_buzz(1)).toEqual(1);
+  beforeEach(function() {
+  game = new fizz_buzz();
+  });
+
+    it("returns 1 if number = 1", function() {
+    expect(game.fizz_buzz(1)).toEqual('1');
     });
 
-    it("returns 'fizz' if number is divisible by 3", function() {
-      expect(fizz_buzz(9)).toEqual('fizz');
+    it("returns 'fizz' if number = 3", function() {
+      expect(game.fizz_buzz(9)).toEqual('fizz');
     });
 
-    it("returns 'buzz' if number is divisible by 5", function() {
-      expect(fizz_buzz(20)).toEqual('buzz');
+    it("returns 'buzz' if number = 5", function() {
+      expect(game.fizz_buzz(20)).toEqual('buzz');
     });
 
-    it("returns 'fizzbuzz' if number is divisible by 15", function() {
-      expect(fizz_buzz(30)).toEqual('fizzbuzz');
+    it("returns 'fizzbuzz' if number = 15", function() {
+      expect(game.fizz_buzz(30)).toEqual('fizzbuzz');
     });
 
-    it("shows 'try again' number must be greater than 0", function() {
-      expect(fizz_buzz(0)).toThrow(new Error('Try again your number must be greater than 0'));
-    });
+      // it("shows 'try again' number must be greater than 0", function() {
+      //    expect(game.fizz_buzz(0)).toThrow(new Error('Try again your number must be greater than 0'));
+      //  });
 });
