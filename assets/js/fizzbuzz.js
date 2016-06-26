a4a7319f892f69e5fa6780910c62b77e11890afb
lollypop27 <lara@adafctory.co.za>
2016-06-26 17:57:55 +0200

    simplyfiying output code

--- assets/js/fizzbuzz.js
+++ assets/js/fizzbuzz.js
@@ -1,27 +1,13 @@
 function fizz_buzz(){
     for(var i = 1; i <=100; i++){
-        if(i % 3 === 0 && i % 5 === 0) {
-            console.log(i+' fizzbuzz');
-        } else if(i % 3 === 0) {
-            console.log(i+' fizz');
-        } else if(i % 5 ===0) {
-            console.log(i+' buzz');
+        if(i % 15 == 0) {
+            console.log(i + ' fizzbuzz');
+        } else if(i % 3 == 0) {
+            console.log(i + ' fizz');
+        } else if(i % 5 == 0) {
+            console.log(i + ' buzz');
         } else {
             console.log(i);
         }
     }
 } fizz_buzz();
-
-// function fizz_buzz(number) {
-//   if (number <= 0) {
-//     return new Error("Try again your number must be greater than 0");
-//   else if (nummber % 3 == 0) {
-//       obj.Message = "fizz"; }
-//   else if (number % 5 == 0) {
-//       obj,Message = "buzz"; }
-//   else if (nummber % 15 == 0) {
-//       obj.Message = "fizzbuzz"; }
-//   else {
-//     return = number;
-//   }
-// };
