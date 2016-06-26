58c9b58fb8d6f4f2afcf30eb8d3a5eb4a19356f9
lollypop27 <lara@adafctory.co.za>
2016-06-26 09:43:41 +0200

    adding if number is not greater than 0 return try again

--- assets/js/fizzbuzz.js
+++ assets/js/fizzbuzz.js
@@ -1,10 +1,13 @@
 function fizz_buzz(number) {
-
-    if (nummber % 15 == 0) {
-        obj.Message = "fizzbuzz" }
-    else if (nummber % 3 == 0) {
-        obj.Message = "fizz" }
-    else if (number % 5 == 0) {
-        obj,Message = "buzz" }
-    }
+  if (number <= 0) {
+    return new Error("Try again your number must be greater than 0");
+  else if (nummber % 15 == 0) {
+      obj.Message = "fizzbuzz"; }
+  else if (nummber % 3 == 0) {
+      obj.Message = "fizz"; }
+  else if (number % 5 == 0) {
+      obj,Message = "buzz"; }
+  else {
+    obj.Message = number;
+  }
 };
--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -1,6 +1,10 @@
 describe("fizz_buzz", function() {
     var fizz_buzz;
 
+    it("returns number if no conditions are met", function() {
+      expect(fizz_buzz(1)).toEqual(1);
+    });
+
     it("returns 'fizz' if number is divisible by 3", function() {
       expect(fizz_buzz(9)).toEqual('fizz');
     });
@@ -11,5 +15,9 @@ describe("fizz_buzz", function() {
 
     it("returns 'fizzbuzz' if number is divisible by 15", function() {
       expect(fizz_buzz(30)).toEqual('fizzbuzz');
+    });
+
+    it("shows 'try again' number must be greater than 0", function() {
+      expect(fizz_buzz(0)).toThrow(new Error('Try again your number must be greater than 0'));
     });
 });
