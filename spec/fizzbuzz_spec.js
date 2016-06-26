9ef2fe920fbaf07f65a50982fde5d0fc207f45c7
lollypop27 <lara@adafctory.co.za>
2016-06-26 09:07:14 +0200

    adding buzz % 5 equation

--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -2,6 +2,10 @@ describe("fizz_buzz", function() {
     var fizz_buzz;
 
     it("returns 'fizz' if number is divisible by 3", function() {
-      expect(fizz_buzz(3)).toEqual('Fizz');
+      expect(fizz_buzz(9)).toEqual('fizz');
+    });
+
+    it("returns 'buzz' if number is divisible by 5", function() {
+      expect(fizz_buzz(20)).toEqual('buzz');
     });
 });
